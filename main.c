a6cb20e359a343a36627bc1b8ea714b2df7f130c
Izaak Meckler <ihmeckler@gmail.com>
2020-12-02 19:56:10 -0800

    print nice thing

--- main.c
+++ main.c
@@ -60,7 +60,7 @@ int main(int argc, char* argv[]) {
   char* actual_memo = "this is a memo";
   size_t actual_memo_len = strlen(actual_memo);
   txn.memo[0] = 1;
-  txn.memo[1] = 14;
+  txn.memo[1] = actual_memo_len; // length
   for (size_t i = 0; i < actual_memo_len; ++i) {
     txn.memo[2 + i] = actual_memo[i];
   }
@@ -68,9 +68,13 @@ int main(int argc, char* argv[]) {
     txn.memo[i] = 0;
   }
 
+  char* fee_payer_str = "B62qiy32p8kAKnny8ZFwoMhYpBppM1DWVCqAPBYNcXnsAHhnfAAuXgg";
+  char* source_str = "B62qiy32p8kAKnny8ZFwoMhYpBppM1DWVCqAPBYNcXnsAHhnfAAuXgg";
+  char* receiver_str = "B62qrcFstkpqXww1EkSGrqMCwCNho86kuqBd4FrAAUsPxNKdiPzAUsy";
+
   txn.fee = 3;
   txn.fee_token = 1;
-  read_public_key_compressed(&txn.fee_payer_pk, "B62qiy32p8kAKnny8ZFwoMhYpBppM1DWVCqAPBYNcXnsAHhnfAAuXgg");
+  read_public_key_compressed(&txn.fee_payer_pk, fee_payer_str);
   txn.nonce = 200;
   txn.valid_until = 10000;
 
@@ -78,8 +82,8 @@ int main(int argc, char* argv[]) {
   txn.tag[1] = 0;
   txn.tag[2] = 0;
 
-  read_public_key_compressed(&txn.source_pk, "B62qiy32p8kAKnny8ZFwoMhYpBppM1DWVCqAPBYNcXnsAHhnfAAuXgg");
-  read_public_key_compressed(&txn.receiver_pk, "B62qrcFstkpqXww1EkSGrqMCwCNho86kuqBd4FrAAUsPxNKdiPzAUsy");
+  read_public_key_compressed(&txn.source_pk, source_str);
+  read_public_key_compressed(&txn.receiver_pk, receiver_str);
   txn.token_id = 1;
   txn.amount = 42;
   txn.token_locked = false;
@@ -91,8 +95,33 @@ int main(int argc, char* argv[]) {
   Signature sig;
   sign(&sig, &kp, &txn);
 
-  char buf[DIGITS] = { 0 };
+  char field_str[DIGITS] = { 0 };
+  char scalar_str[DIGITS] = { 0 };
   uint64_t tmp[4];
+  fiat_pasta_fp_from_montgomery(tmp, sig.rx);
+  bigint_to_string(field_str, tmp);
+
+  fiat_pasta_fq_from_montgomery(tmp, sig.s);
+  bigint_to_string(scalar_str, tmp);
+
+  printf("{ publicKey: '%s',\n", fee_payer_str);
+  printf("  signature:\n");
+  printf("   { field:\n");
+  printf("      '%s',\n", field_str);
+  printf("     scalar:\n");
+  printf("      '%s' },\n", scalar_str);
+  printf("  payload:\n");
+  printf("   { to: '%s',\n", receiver_str);
+  printf("     from: '%s',\n", source_str);
+  printf("     fee: '%lu',\n", txn.fee);
+  printf("     amount: '%lu',\n", txn.amount);
+  printf("     nonce: '%u',\n", txn.nonce);
+  printf("     memo: '%s',\n", txn.memo); // TODO: This should actually be b58 encoded
+  printf("     validUntil: '%u' } }\n", txn.valid_until);
+
+  printf("\nsignature only:\n");
+
+  char buf[DIGITS] = { 0 };
 
   fiat_pasta_fp_from_montgomery(tmp, sig.rx);
   bigint_to_string(buf, tmp);
