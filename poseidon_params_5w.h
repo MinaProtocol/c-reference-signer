e34c04e1130dfb9cd3a3ecf4bcafe8d730c3b834
Joseph Spadavecchia <joseph@redtrie.com>
2021-04-15 12:41:56 +0100

    Updated 5-wire Poseidon full rounds according to Vanishree's calculations

--- poseidon_params_5w.h
+++ poseidon_params_5w.h
@@ -2,7 +2,7 @@
 // 5-wire Poseidon hash function parameters
 //
 
-#define ROUND_COUNT_5W  61
+#define ROUND_COUNT_5W  53
 #define SPONGE_WIDTH_5W 5
 #define SPONGE_RATE_5W  4
 #define SBOX_ALPHA_5W   7
